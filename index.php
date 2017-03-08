598f71134dfe84e1193b79d186b9a914fb0909c9
asdas <asd@asd.com>
2017-03-08 14:11:41 -0500

    Index
    McLovin

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    if (isset($_REQUEST["c"])) {
+        $controller = strtolower($_REQUEST["c"]);
+        $action = isset($_REQUEST["a"]) ? $_REQUEST["a"] : "mainPage";
+
+        require_once "controller/$controller.controller.php";
+        $controller = ucwords($controller).'Controller';
+
+        $controller = new $controller;
+
+        call_user_func(array($controller, $action));
+    } else {
+        $controller = "main";
+        require "controller/$controller.controller.php";
+
+        $controller = ucwords($controller).'Controller';
+        $controller = new $controller;
+
+        $controller->mainPage();
+    }
+
+    if (isset($_GET["msn"])) {
+        echo "<script>('".$_GET["msn"]."')</script>";
+    }
+?>
