a513ebbf5d6e8486544b8ad15948ce56bc724e80
ciego1898 <daniel.eco18@gmail.com>
2017-03-08 21:19:45 -0500

    Conexion nueva

--- model/conn.model.php
+++ model/conn.model.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/origin/Daniel_Cardona
     class DataBase{
         private static $db_host = "localhost";
         private static $db_name = "mentalidadfitness";
@@ -11,16 +8,11 @@
         private static $db_conn = null;
 
         public static function connect(){
-<<<<<<< HEAD
-            if(self::$db_conn==null){
-=======
             if (self::$db_conn==null) {
->>>>>>> refs/remotes/origin/Daniel_Cardona
                 try {
                     self::$db_conn = new PDO("mysql:host=".self::$db_host.";dbname=".self::$db_name,self::$db_user,self::$db_pass);
                     self::$db_conn->exec("SET CHARACTER SET utf8");
                 } catch (PDOException $e) {
-<<<<<<< HEAD
                     die($e->getMessage()."".$e->getLine()."".$e->getFile());
                 }
             }
@@ -28,17 +20,7 @@
         }
 
         public static function disconnect(){
-            self::$db_conn=null;
-=======
-                    die($e->getMessage()." ".$e->getLine()." ".$e->getFile());
-                }
-                return self::
-            }
-        }
-
-        public static function disconnect(){
             self::$db_conn = null;
->>>>>>> refs/remotes/origin/Daniel_Cardona
         }
     }
 
