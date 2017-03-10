da6d60dbdb7739324a883107f98e3fef64ba2225
assadsa <asdasd34@misenasdaa.edu.co>
2017-03-10 12:03:34 -0500

    Main

--- views/modules/mod_comida/comida.add.php
+++ views/modules/mod_comida/comida.add.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            <h1>GESTIONAR COMIDA</h1>
+            <h1>GESTIONAR COMIDAS</h1>
         </div>
         <div class="col-sm-4 col-sm-offset-4">
             <form class="" action="?c=comida&a=create" method="post">
