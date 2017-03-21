78776c30411cefabce4d5b4509f60b082930ac77
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-03-21 13:01:06 -0500

    login

--- views/pages/login.php
+++ views/pages/login.php
@@ -4,7 +4,7 @@
         <div class="form-group" id="campo1">
           <input type="text" class="form-control" placeholder="Email" name="data[]">
         </div>
-        <div class="form-group" id="campo2">
+        <div class="form-group">
           <input type="password" class="form-control" placeholder="Contraseña" name="data[]">
         </div>
         <button>ENVIAR</button>
