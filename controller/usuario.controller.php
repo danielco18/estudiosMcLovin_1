d761f56cb704ea11d4a5cee6e0c44ad7d6bea8a0
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-03-22 12:40:41 -0500

    AAAWAWAWA
    FAVO ME DA UN PASTEL WAWAWA

--- controller/usuario.controller.php
+++ controller/usuario.controller.php
@@ -25,9 +25,30 @@ session_start();
             $n=3;
             $s="Juan";
             $data = $_POST["data"];
-            $data[2] = password_hash($data[2],PASSWORD_DEFAULT);
-            $result = $this->USmodel->createUsuario($userId,$data,$tokken,$n,$s);
-            header("Location: index.php?c=usuario&msn=$result");
+            if (strlen($data[2])<=8){
+              $msn="La contraseña debe tener mas de 8 caracteres";
+              header("Location: index.php?c=usuario&msn=$msn");
+            }
+            elseif(!preg_match('`[a-z]`',$data[2])) {
+              $msn="La contraseña debe tener minimo una miniscula";
+              header("Location: index.php?c=usuario&msn=$msn");
+            }
+            elseif(!preg_match('`[0-9]`',$data[2])) {
+              $msn="La contraseña debe tener minimo un numero";
+              header("Location: index.php?c=usuario&msn=$msn");
+            }
+            /*elseif(!preg_match('`[/\*+-%&@¡!|]`',$data[2])) {
+              $msn="La contraseña debe tener minimo un simbolo";
+              header("Location: index.php?c=usuario&msn=$msn");
+            }*/elseif($data[2]!==$data[5]) {
+              $msn="La contraseñas no coinciden";
+              header("Location: index.php?c=usuario&msn=$msn");
+            }
+            else{
+              $data[2] = password_hash($data[2],PASSWORD_DEFAULT);
+              $result = $this->USmodel->createUsuario($userId,$data,$tokken,$n,$s);
+              header("Location: index.php?c=usuario&msn=$result");
+            }
         }
 
         public function update(){
